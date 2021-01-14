c3da1f3af87300032c2a09af3c75a1bdc6054aaa
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-14 08:53:35 +0700

    1

---
+++ PT16311_WEB_NET101_SP21/B/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B
+{
+    class Program
+    {
+        /*
+         * Bài 1.6 Cầu điều kiện IF...ELSE (Nếu Thì)
+         * Dùng để thực hiện 1 hành động nếu IF(TRUE) và ngược lại sẽ vào Else.
+         * Cấu trúc: 
+         * if (true)
+            {
+                Nếu mà If true thì sẽ thực hiện 1 hành động nào đó
+            }
+            else
+            {
+                Nếu không thỏa mãn IF thì mặc định là vào ELSE
+            }
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            //Ví dụ: Viết 1 chương trình nhập số buổi nghỉ và điểm thi với điều kiện như sau. Nếu số buổi nghỉ <=4 thì mới yêu cầu người dùng nhập điểm thi còn ngược lại thì in ra thông báo bạn không đủ điều kiện nhập điểm thi.
+
+            //Bước 1: Xác định biến cần sử dụng
+            int nghi;
+            double diemCSharp;
+            //Bước 2: Gán giá trị cho biến
+            Console.WriteLine("Mời bạn nhập số buổi nghỉ: ");
+            nghi = Convert.ToInt32(Console.ReadLine());
+            //Bước 3: Xử lý nghiệp vụ
+            if (nghi <= 4 && nghi >= 0)
+            {
+                Console.WriteLine("Mời bạn nhập điểm vào: ");
+                diemCSharp = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Buổi nghỉ {0} | Điểm của bạn: {1}", nghi, diemCSharp);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Bạn nghỉ {0} vì vậy không thể nhập điểm thi", nghi);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PT16311_WEB_NET101_SP21/Bai_1.8_ToanTuBaNgoi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1._8_ToanTuBaNgoi
+{
+    class Program
+    {
+        /*
+         * TOÁN TỬ 3 NGÔI
+         * <Điều kiện> ? <Kết quả nếu điều kiện đúng> : <Kết quả nếu điều kiện sai>;
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
+            // Toán tử 3 ngôi
+            string kq = (5 > 6) || (3 > 2) ? "Đúng" : "Sai";
+            Console.WriteLine(kq);
+            //Viết kiểu if else
+            if (5 > 6)
+            {
+                Console.WriteLine("Đúng");
+            }
+            else
+            {
+                Console.WriteLine("Sai");
+            }
+            Console.ReadKey();
+        }
+    }
+}
