b81bdf18c42ff1d43caaa37a66bb6bdea7378cb5
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-23 11:18:25 +0700

    1

---
+++ PT16311_WEB_NET101_SP21/Bai_3.0_OOP_LapTrinhHuongDoiTuong/SinhVien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3._0_OOP_LapTrinhHuongDoiTuong
+{
+    /*
+     * Tạo ra 1 đối tượng Sinh Viên
+     *
+     * Tên Class bắt buộc phải viết hoa chữ cái đầu của từ lên
+     */
+    class SinhVien
+    {
+        //Phần 1: Liệt kê tất cả các thuộc tính mà đối tượng phải có
+        public string tenSV;
+        public string queQuanSv;
+        public string diachi;
+        public int namsinh;
+        public int sdt;
+
+        //Phần 2: Contructor
+
+
+        //Phần 3: Phương thức - method
+        public void getSinhVien()
+        {
+            Console.WriteLine("Chào mừng {0} {1}", tenSV, queQuanSv);
+        }
+    }
+}
